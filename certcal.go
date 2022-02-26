137b4eb06f46cac6487f057509df028dcca8edff
Daniel Schauenberg <d@unwiredcouch.com>
2022-02-26 13:15:13 +0000

    use kong for cli flags

--- certcal.go
+++ certcal.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"github.com/alecthomas/kong"
 	"github.com/mrtazz/certcal/handler"
 	"github.com/mrtazz/certcal/hosts"
 	log "github.com/sirupsen/logrus"
 	"net/http"
-	"os"
-	"strings"
 	"time"
 )
+
+var (
+	version   = ""
+	goversion = ""
+)
+
+// CLI defines the command line arguments
+var CLI struct {
+	Serve struct {
+		Hosts    []string `required:"" help:"hosts to check certs for." env:"CERTCAL_HOSTS"`
+		Interval string   `required:"" help:"interval in which to check certs" env:"CERTCAL_INTERVAL" default:"24h"`
+		Port     int      `help:"port for the server to listen on" env:"PORT" default:"3000"`
+	} `cmd:"" help:"run the server."`
+}
 
 func main() {
 
@@ -19,42 +33,28 @@ func main() {
 		"package": "main",
 	})
 
-	checkHosts := os.Getenv("CERTCAL_HOSTS")
-	if checkHosts == "" {
-		logger.Error("missing env var CERTCAL_HOSTS")
-		os.Exit(1)
-	}
-
-	var duration time.Duration
-	durationString := os.Getenv("CERTCAL_INTERVAL")
-	switch durationString {
-	case "":
-		logger.Error("missing env var CERTCAL_INTERVAL, defaulting to 1 day")
-		duration = 24 * time.Hour
-	default:
-		var err error
-		duration, err = time.ParseDuration(durationString)
+	ctx := kong.Parse(&CLI)
+	switch ctx.Command() {
+	case "serve":
+		duration, err := time.ParseDuration(CLI.Serve.Interval)
 		if err != nil {
 			logger.Error("unable to parse CERTCAL_INTERVAL, defaulting to 1 day")
 			duration = 24 * time.Hour
 		}
+		hosts.AddHosts(CLI.Serve.Hosts)
+		hosts.UpdateEvery(duration)
+
+		address := fmt.Sprintf(":%d", CLI.Serve.Port)
+
+		logger.WithFields(log.Fields{
+			"address": address,
+		}).Info("starting web server")
+		http.HandleFunc("/hosts", handler.Handler)
+		//http.HandleFunc("/metrics", headers)
+		http.ListenAndServe(address, nil)
+
+	default:
+		logger.Error("Unknown command: " + ctx.Command())
 	}
-
-	var port string
-	if port = os.Getenv("PORT"); port == "" {
-		port = "3000"
-	}
-
-	hosts.AddHosts(strings.Split(checkHosts, ","))
-	hosts.UpdateEvery(duration)
-
-	address := ":" + port
-
-	logger.WithFields(log.Fields{
-		"address": address,
-	}).Info("starting web server")
-	http.HandleFunc("/hosts", handler.Handler)
-	//http.HandleFunc("/metrics", headers)
-	http.ListenAndServe(address, nil)
 
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,10 @@ require (
 )
 
 require (
+	github.com/alecthomas/kong v0.4.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
